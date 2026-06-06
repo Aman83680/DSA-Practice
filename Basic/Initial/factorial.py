bd72b934124389259ccb8c471f679554ed965020
Aman Sharma <aman.sharma.devx@gmail.com>
2026-06-06 21:50:50 +0530

    Refactor factorial function for better handling
    Refactored factorial calculation to handle edge cases and improve readability.

--- Basic/Initial/factorial.py
+++ Basic/Initial/factorial.py
@@ -1,9 +1,10 @@
-number=int(input("Enter the number:-"))
-if number<0:
-    print("not possible")
-else:
-    factorial=1
-    while number > 0:
-     factorial = factorial * number
-     number = number - 1
-    print(factorial)
+n=int(input("Enter the number: "))
+if n<0:
+    print("Factorial not exist for Negative Numbers.")
+elif n==0 or n==1:
+    print("Factorial is: 1")
+elif n>1:
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    print("Factorial is:",fact)
